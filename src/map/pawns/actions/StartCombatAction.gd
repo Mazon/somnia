3cc28236dd1b9097e488a3a0ee1ddd0b09f1b7dc
hardy <hardy@nordiclight.org>
2022-06-05 12:03:35 +0700

    first

---
+++ src/map/pawns/actions/StartCombatAction.gd
@@ -0,0 +1,16 @@
+extends MapAction
+class_name StartCombatAction
+
+export var formation: PackedScene
+
+
+func interact() -> void:
+	get_tree().paused = false
+	local_map.start_encounter(formation)
+	emit_signal("finished")
+
+
+func _on_EncounterPawn_body_entered(body):
+	get_tree().paused = false
+	local_map.start_encounter(formation)
+	emit_signal("finished")
